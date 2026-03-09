b1a9a62f13e16c1550f0082d96a80c9e16e16297
dabin-kang <ekqlskwkd287@gmail.com>
2026-03-09 21:31:47 +0900

    사이트 생성

---
+++ src/main/groovy/org/example/sprprjsite/SprPrjSiteApplication.groovy
@@ -0,0 +1,13 @@
+package org.example.sprprjsite
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SprPrjSiteApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(SprPrjSiteApplication, args)
+    }
+
+}
---
+++ src/test/groovy/org/example/sprprjsite/SprPrjSiteApplicationTests.groovy
@@ -0,0 +1,13 @@
+package org.example.sprprjsite
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SprPrjSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
